58a5b6bb12cd6cdeeb8fffd1ca8b65b24fe06531
MtMadness08 <soupeater56@gmail.com>
2025-01-30 20:30:26 -0500

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 // 1. I have read the syllabus
 // 2. The schedule seemed interesting as it follows a similar pattern to what we covered in COS 140, 
 // and puts them into blocks that take the course of a week.
-// 3. 
+// 3. An object is what every single piece of Java code stems from.
 
 
 public class Test{
