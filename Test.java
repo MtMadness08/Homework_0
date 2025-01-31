1484a35bec996c99360333d78503a720024d2469
MtMadness08 <soupeater56@gmail.com>
2025-01-30 20:31:07 -0500

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,6 +2,7 @@
 // 2. The schedule seemed interesting as it follows a similar pattern to what we covered in COS 140, 
 // and puts them into blocks that take the course of a week.
 // 3. An object is what every single piece of Java code stems from.
+// 4. Repository: https://github.com/MtMadness08/Homework_0
 
 
 public class Test{
