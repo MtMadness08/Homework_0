8e6bb52871a48a3bee683772951cd29720eaed55
MtMadness08 <soupeater56@gmail.com>
2025-01-30 20:33:01 -0500

    Create Homework_0.java

---
+++ Homework_0.java
@@ -0,0 +1,13 @@
+// 1. I have read the syllabus
+// 2. The schedule seemed interesting as it follows a similar pattern to what we covered in COS 140, 
+// and puts them into blocks that take the course of a week.
+// 3. An object is what every single piece of Java code stems from.
+// 4. Repository: https://github.com/MtMadness08/Homework_0
+
+
+public class Test{
+    public static void main(String[] args){
+        Test x = new Test();
+        System.out.println("Hello World");
+    }
+}
